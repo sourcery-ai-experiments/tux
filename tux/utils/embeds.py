940ae9697677cc0ff0f47d425fafc8a3aeec07d0
kzndotsh <admin@kaizen.wtf>
2024-04-02 21:32:33 +0000

    refactor(embeds.py): extract timestamp and footer generation into separate functions for better code organization refactor(embeds.py): simplify embed creation functions by using a common create_embed function to reduce code duplication

--- tux/utils/embeds.py
+++ tux/utils/embeds.py
@@ -1,40 +1,57 @@
+import datetime
+
 import discord
 from discord.ext import commands
 
 from tux.utils.constants import Constants as CONST
 
 
+def get_timestamp(
+    ctx: commands.Context[commands.Bot] | None, interaction: discord.Interaction | None
+) -> datetime.datetime:
+    if ctx and ctx.message:
+        return ctx.message.created_at
+    return interaction.created_at if interaction else discord.utils.utcnow()
+
+
+def get_footer(
+    ctx: commands.Context[commands.Bot] | None, interaction: discord.Interaction | None
+) -> tuple[str, str]:
+    footer = ("", "")
+    if ctx:
+        footer = (f"Requested by {ctx.author.display_name}", ctx.author.display_avatar.url)
+    elif interaction:
+        footer = (
+            f"Requested by {interaction.user.display_name}",
+            interaction.user.display_avatar.url,
+        )
+    return footer
+
+
 def base_embed(
-    ctx: commands.Context[commands.Bot] | None,
-    interaction: discord.Interaction | None,
-    state: str,
+    ctx: commands.Context[commands.Bot] | None, interaction: discord.Interaction | None, state: str
 ) -> discord.Embed:
     embed = discord.Embed()
 
     embed.color = CONST.EMBED_STATE_COLORS[state]
-
     embed.set_author(name=state.capitalize(), icon_url=CONST.EMBED_STATE_ICONS[state])
+    embed.timestamp = get_timestamp(ctx, interaction)
+    footer = get_footer(ctx, interaction)
+    embed.set_footer(text=footer[0], icon_url=footer[1])
 
-    if ctx and ctx.message:
-        embed.timestamp = ctx.message.created_at
-    elif interaction:
-        embed.timestamp = interaction.created_at
-    else:
-        embed.timestamp = discord.utils.utcnow()
+    return embed
 
-    if ctx:
-        embed.set_footer(
-            text=f"Requested by {ctx.author.display_name}",
-            icon_url=ctx.author.display_avatar.url,
-        )
-    elif interaction:
-        embed.set_footer(
-            text=f"Requested by {interaction.user.display_name}",
-            icon_url=interaction.user.display_avatar.url,
-        )
-    else:
-        embed.set_footer(text="", icon_url="")
 
+def create_embed(
+    ctx: commands.Context[commands.Bot] | None,
+    interaction: discord.Interaction | None,
+    state: str,
+    title: str,
+    description: str,
+) -> discord.Embed:
+    embed = base_embed(ctx, interaction, state)
+    embed.title = title
+    embed.description = description
     return embed
 
 
@@ -44,12 +61,7 @@ def create_default_embed(
     title: str,
     description: str,
 ) -> discord.Embed:
-    embed = base_embed(ctx, interaction, "DEFAULT")
-
-    embed.title = title
-    embed.description = description
-
-    return embed
+    return create_embed(ctx, interaction, "DEFAULT", title, description)
 
 
 def create_info_embed(
@@ -58,12 +70,7 @@ def create_info_embed(
     title: str,
     description: str,
 ) -> discord.Embed:
-    embed = base_embed(ctx, interaction, "INFO")
-
-    embed.title = title
-    embed.description = description
-
-    return embed
+    return create_embed(ctx, interaction, "INFO", title, description)
 
 
 def create_error_embed(
@@ -72,12 +79,7 @@ def create_error_embed(
     title: str,
     description: str,
 ) -> discord.Embed:
-    embed = base_embed(ctx, interaction, "ERROR")
-
-    embed.title = title
-    embed.description = description
-
-    return embed
+    return create_embed(ctx, interaction, "ERROR", title, description)
 
 
 def create_warning_embed(
@@ -86,12 +88,7 @@ def create_warning_embed(
     title: str,
     description: str,
 ) -> discord.Embed:
-    embed = base_embed(ctx, interaction, "WARNING")
-
-    embed.title = title
-    embed.description = description
-
-    return embed
+    return create_embed(ctx, interaction, "WARNING", title, description)
 
 
 def create_success_embed(
@@ -100,9 +97,4 @@ def create_success_embed(
     title: str,
     description: str,
 ) -> discord.Embed:
-    embed = base_embed(ctx, interaction, "SUCCESS")
-
-    embed.title = title
-    embed.description = description
-
-    return embed
+    return create_embed(ctx, interaction, "SUCCESS", title, description)
